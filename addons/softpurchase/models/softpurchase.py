e632d494d0a66ed1612e3a7e07093889a0f9a36e
mohamed ali jenni <jennimohamedali111@gmail.com>
2017-07-19 10:39:08 -0400

    modification 1 softpurchase

--- addons/softpurchase/models/softpurchase.py
+++ addons/softpurchase/models/softpurchase.py
@@ -11,6 +11,16 @@ class softpurchase(models.Model):
         ('done', 'Locked'),
         ('cancel', 'Cancelled')
         ], string='Status', readonly=True, index=True, default='prepurchase',copy=False,track_visibility='onchange')
+
+    @api.multi
+    def button_approve(self, force=False):
+        self.write({'state': 'purchase'})
+        self.write({'monstate': 'purchase'})
+        self._create_picking()
+        if self.company_id.po_lock == 'lock':
+            self.write({'state': 'done'})
+            self.write({'monstate': 'done'})
+        return {}
     
        
     
